97c05b63984a78a33f3fd40711096b411c7760e4
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-08-13 13:13:13 -0600

    Feat: Add header.

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,6 +1,12 @@
 const Header = () => {
   return (
-    <div>Header...</div>
+    <header>
+      <div>
+        <a>
+          <img src="/images/xora.svg" width={115} height={55} alt="logo"/>
+        </a>
+      </div>
+    </header>
   )
 }
 
