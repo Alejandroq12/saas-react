0af06aff7d8452398c8552208ec9c66c2a9174f3
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-08-17 13:13:13 -0600

    Refactor: Improve header.

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,3 +1,14 @@
+import { Link as LinkScroll } from 'react-scroll';
+
+const NavLink = ({ title }) => (
+  <LinkScroll
+  className='base-bold text-p4 uppercase transition-colors duration-500
+  cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'
+  >
+    {title}
+  </LinkScroll>
+);
+
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-10">
@@ -9,7 +20,27 @@ const Header = () => {
         <div className="w-full border-2 border-amber-400">
           <nav>
             <ul className="flex max-lg:block max-lg:px-12">
-              <li className="nav-li">{/* <NavLink /> */}</li>
+              <li className="nav-li">
+                <NavLink title="features" />
+                <div className="dot" />
+                <NavLink title="pricing" />
+              </li>
+              <li className="nav-logo">
+                <LinkScroll>
+                  <img
+                    src="/images/xora.svg"
+                    width={160}
+                    height={55}
+                    alt="logo"
+                  />
+                </LinkScroll>
+              </li>
+
+              <li className="nav-li">
+                <NavLink title="faq" />
+                <div className="dot" />
+                <NavLink title="download" />
+              </li>
             </ul>
           </nav>
         </div>
