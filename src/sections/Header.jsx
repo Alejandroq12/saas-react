fec25e5396571cf2c7bd610e9b62357212fddab2
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-08-18 13:13:13 -0600

    Refactor: Improve header.

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -2,8 +2,8 @@ import { Link as LinkScroll } from 'react-scroll';
 
 const NavLink = ({ title }) => (
   <LinkScroll
-  className='base-bold text-p4 uppercase transition-colors duration-500
-  cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'
+    className="base-bold text-p4 uppercase transition-colors duration-500
+  cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
   >
     {title}
   </LinkScroll>
@@ -17,32 +17,34 @@ const Header = () => {
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
 
-        <div className="w-full border-2 border-amber-400">
-          <nav>
-            <ul className="flex max-lg:block max-lg:px-12">
-              <li className="nav-li">
-                <NavLink title="features" />
-                <div className="dot" />
-                <NavLink title="pricing" />
-              </li>
-              <li className="nav-logo">
-                <LinkScroll>
-                  <img
-                    src="/images/xora.svg"
-                    width={160}
-                    height={55}
-                    alt="logo"
-                  />
-                </LinkScroll>
-              </li>
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-5">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                <li className="nav-li">
+                  <NavLink title="features" />
+                  <div className="dot" />
+                  <NavLink title="pricing" />
+                </li>
+                <li className="nav-logo">
+                  <LinkScroll>
+                    <img
+                      src="/images/xora.svg"
+                      width={160}
+                      height={55}
+                      alt="logo"
+                    />
+                  </LinkScroll>
+                </li>
 
-              <li className="nav-li">
-                <NavLink title="faq" />
-                <div className="dot" />
-                <NavLink title="download" />
-              </li>
-            </ul>
-          </nav>
+                <li className="nav-li">
+                  <NavLink title="faq" />
+                  <div className="dot" />
+                  <NavLink title="download" />
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
