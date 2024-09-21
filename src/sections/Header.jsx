014cf956a8d8856759e662ec5a67015fa7121217
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-09-21 17:02:53 -0600

    Refactor: Improve header.

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
 
-        <div className={clsx("w-full max-lg:fixed max-lg:top-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0", isOpen ? "")}>
+        <div className={clsx("w-full max-lg:fixed max-lg:top-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0", isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none")}>
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-5">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
@@ -30,7 +30,13 @@ const Header = () => {
                   <NavLink title="pricing" />
                 </li>
                 <li className="nav-logo">
-                  <LinkScroll>
+                  <LinkScroll
+                  to="hero"
+                  offset={-100}
+                  spy
+                  smooth
+                  className={clsx("max-lg:hidden trasition-transform duration-500 cursor-pointer")}
+                  >
                     <img
                       src="/images/xora.svg"
                       width={160}
